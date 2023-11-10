a4258daab8d8d933672b256c58309b0a9e138538
cd-con <qqdev@yandex.ru>
2023-11-10 16:14:09 +0300

    Начал создавать легендарную игру с корованами

---
+++ Lib_11/Practice8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_11
+{
+    public interface Human
+    {
+        public string Name { get; set; }
+
+        public string Talk();
+    }
+
+    public class Batya : Human
+    {
+        private string _name;
+        public string Name { get => _name; set => _name = value; }
+
+        public Batya(string name) => Name = name;
+
+        public string Talk() => $"Меня зовут {Name}";
+    }
+
+    public class SinaKorzina : Human
+    {
+        public Batya batya;
+
+        private string _name;
+        public string Name { get => _name; set => _name = value; }
+        public string MidName;
+
+        public SinaKorzina(string name, Batya batya, string midName = null)
+        {
+            this.batya = batya;
+            Name = name;
+            MidName = midName;
+        }
+
+        public string Talk() => MidName == null ? $"Меня зовут {Name}, а моего батю - {batya.Name}" : $"Меня зовут {Name} {MidName}";
+    }
+}
---
+++ Practice8_var11/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Lib_11;
+
+namespace Practice8_var11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public float money = 0f;
+        public int pivo = 0;
+        public int candies = 0;
+
+        Batya batya = new("Валера");
+        SinaKorzina son;
+        public MainWindow()
+        {
+            InitializeComponent();
+            son = new("Валерик", batya, "Валерьивич");
+        }
+
+        private void BatyaTalkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pivo > 1)
+            {
+                pivo--;
+                MessageBox.Show(batya.Talk());
+            }
+            else
+            {
+                MessageBox.Show("А ГДЕ ПИВО??");
+            }
+
+        }
+    }
+}
